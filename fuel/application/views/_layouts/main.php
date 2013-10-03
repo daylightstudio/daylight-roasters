7dd74a554e6ce964e10c4f300b94c47a44d1f631
Piet van Zoen <pietvanzoen@gmail.com>
2013-10-03 14:31:44 -0700

    Remove secondary block and adjust main layout.

--- fuel/application/views/_layouts/main.php
+++ fuel/application/views/_layouts/main.php
@@ -1,13 +1,7 @@
 <?php $this->load->view('_blocks/header')?>
-	
+
 	<div class="main_container wrapper">
-		<article class="primary_content">
-			<?=fuel_var('body', ''); ?>
-		</article>
-		
-		<aside class="secondary_content">
-			<?=fuel_block('secondary')?>
-		</aside>
+		<?=fuel_var('body', ''); ?>
 	</div>
-	
+
 <?php $this->load->view('_blocks/footer')?>
