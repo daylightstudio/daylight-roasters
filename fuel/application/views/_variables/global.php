af56885d707f7803f4312159c2bd699d1efbfa36
Piet van Zoen <pietvanzoen@gmail.com>
2013-10-03 14:35:09 -0700

    Clean up global vars file.

--- fuel/application/views/_variables/global.php
+++ fuel/application/views/_variables/global.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 
 // declared here so we don't have to in each controller's variable file
 $CI =& get_instance();
 
-// generic global page variables used for all pages
+/* GENERIC GLOBAL PAGE VARIABLES USED FOR ALL PAGES
+---------------------------------------- **/
 $vars = array();
 $vars['layout'] = 'main';
 $vars['page_title'] = fuel_nav(array('render_type' => 'page_title', 'delimiter' => ' : ', 'order' => 'desc', 'home_link' => 'Home'));
@@ -13,8 +14,13 @@ $vars['js'] = array();
 $vars['css'] = array();
 $vars['body_class'] = uri_segment(1).' '.uri_segment(2);
 
-// page specific variables
-$pages = array();
 
-include('nav.php'); 
+/* SET $h1 BASED ON NAV LABEL */
+include('nav.php');
 $vars['h1'] = isset($nav[uri_string()]) ? $nav[uri_string()]['label'] : '';
+
+
+/* PAGE SPECIFIC VARS
+---------------------------------------- **/
+$pages = array();
+$pages['home'] = array('body_class' => 'home');
