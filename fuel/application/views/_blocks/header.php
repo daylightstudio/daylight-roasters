3955471330437a02a5dd81912b176d5560a48ebd
Kristin Wille <kristin@bydaylight.com>
2014-03-05 11:09:01 -0800

    everything into SASS!

--- fuel/application/views/_blocks/header.php
+++ fuel/application/views/_blocks/header.php
@@ -1,7 +1,5 @@
 <!doctype html>
-
 <html lang="en">
-
 <head>
 
 	<?php if (!empty($is_blog)) : ?>
@@ -13,7 +11,7 @@
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 
 	<?php if (!empty($meta_keywords)): ?>
 	<meta name="keywords" content="<?php echo fuel_var('meta_keywords')?>" />
